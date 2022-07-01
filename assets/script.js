0f737472e28949d926edfa7ededbf5b03dc165e8
E73707 <94885681+E73707@users.noreply.github.com>
2022-07-01 17:44:26 +1000

    added js script and modified Git portion of the html file

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (var x = 0; x < topics.length; x++){
+    console.log(topics[x])
+}
+/*
+if (topic === 'HTML'){
+    console.log("lets study HTML!");
+} else if (topic === 'CSS'){
+    console.log("Lets study CSS!")
+} else if (topic === 'Git'){
+    console.log("lets study Git!")
+} else if (topic === 'JavaScript'){
+    console.log("lets study JavaScript!")
+} else {
+    console.log("Please try again!");
+}
+*/
+
